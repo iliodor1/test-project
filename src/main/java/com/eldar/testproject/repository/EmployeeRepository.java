a2efd6a6e22f53527f0e383b485007c5f0c725e9
Eldar <eldar.gai@yandex.ru>
2022-06-27 20:17:42 +0500

    feat: add EmployeeRepository

---
+++ src/main/java/com/eldar/testproject/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.eldar.testproject.repository;
+
+import com.eldar.testproject.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Override
+    List<Employee> findAll();
+
+    Optional<Employee> findEmployeeById(Long id);
+}
