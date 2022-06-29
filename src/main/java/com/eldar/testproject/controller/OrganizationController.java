2352522c64f470c2c613523c4c516c69d400db50
Eldar <eldar.gai@yandex.ru>
2022-06-29 14:12:26 +0500

    refactor: add OrganizationController constructor

--- src/main/java/com/eldar/testproject/controller/OrganizationController.java
+++ src/main/java/com/eldar/testproject/controller/OrganizationController.java
@@ -1,22 +1,21 @@
 package com.eldar.testproject.controller;
 
-import com.eldar.testproject.entity.Employee;
 import com.eldar.testproject.entity.Organization;
-import com.eldar.testproject.service.EmployeeService;
 import com.eldar.testproject.service.OrganizationService;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/organizations")
-@AllArgsConstructor
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequestMapping("organizations")
 public class OrganizationController {
-    OrganizationService organizationService;
+    private final OrganizationService organizationService;
+
+    @Autowired
+    public OrganizationController(OrganizationService organizationService) {
+        this.organizationService = organizationService;
+    }
 
     @GetMapping("/{id}")
     public Organization getOrganization(@PathVariable Long id) {
@@ -29,12 +28,12 @@ public class OrganizationController {
     }
 
     @PostMapping
-    public Organization createOrganization(Organization organization) {
+    public Organization createOrganization(@RequestBody Organization organization) {
         return organizationService.createOrganization(organization);
     }
 
     @PutMapping
-    public Organization updateOrganization(Organization organization) {
+    public Organization updateOrganization(@RequestBody Organization organization) {
         return organizationService.updateOrganization(organization);
     }
 
